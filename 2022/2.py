3e4223dc3c3717e72076b7f8a2ac187d11ee355c
ruhal <ruhalmiah@gmail.com>
2023-12-01 21:08:54 +0000

    2

--- 2022/2.py
+++ 2022/2.py
@@ -1,10 +1,7 @@
 input_file = open("input.txt"  , 'r').read().split('\n')
-
-
 opp = {'A':0, 'B':1, 'C':2}
 me = {'X':0, 'Y':1, 'Z':2}
 
-    
 def round_points(line):
     opp_move = opp[line[0]]
     me_move = me[line[-1]]
@@ -14,11 +11,9 @@ def round_points(line):
         return 0 + me_move + 1
     else:
         return 6 + me_move + 1
-    
-    
+
 points = [round_points(line) for line in input_file]
 print(sum(points))
-
 
 def round_points_2(line):
     opp_move = opp[line[0]]
