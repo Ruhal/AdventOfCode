4a99993e3a12b2a4153fdad2da4af23b263071ab
ruhal <ruhalmiah@gmail.com>
2023-12-02 11:04:22 +0000

    2023 day 2

--- 2023/2.py
+++ 2023/2.py
@@ -1,24 +1,21 @@
 input_file = open("input.txt"  , 'r').read().split('\n')
-colour_count = {"red":12, "green":13, "blue":14}     
+colour_count = {"red":12, "green":13, "blue":14}
 
 def valid_game(i,line):   
     for reveal in line[line.index(":")+2:].split("; "):
         for combo in reveal.split(", "):
             count, colour = combo.split(" ")
-            if int(count) > colour_count[colour]: return 0
-        
+            if int(count) > colour_count[colour]: return 0        
     return i+1
 
 print(sum(valid_game(i,line) for i,line in enumerate(input_file)))
 
 def find_power(line):
-    colour_count = {"red":0, "green":0, "blue":0}
-    
+    colour_count = {"red":0, "green":0, "blue":0}    
     for reveal in line[line.index(":")+2:].split("; "):
         for combo in reveal.split(", "):
             count, colour = combo.split(" ")
-            colour_count[colour] = max(int(count), colour_count[colour])
-        
+            colour_count[colour] = max(int(count), colour_count[colour])        
     return colour_count["red"]*colour_count["green"]*colour_count["blue"]
 
 print(sum(find_power(line) for line in input_file))
